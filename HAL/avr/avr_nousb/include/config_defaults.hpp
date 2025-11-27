14ffdb7abc2e9193c074617e47701d216a065674
Clover Watson <152994098+rhit-watsonpf@users.noreply.github.com>
2025-11-27 12:56:26 -0800

    Updated the SOCD up button

--- HAL/avr/avr_nousb/include/config_defaults.hpp
+++ HAL/avr/avr_nousb/include/config_defaults.hpp
@@ -43,7 +43,7 @@ const Config default_config = {
             .socd_pairs_count = 4,
             .socd_pairs = new SocdPair[4] {
                 SocdPair { .button_dir1 = BTN_LF3, .button_dir2 = BTN_LF1, .socd_type = SOCD_2IP },
-                SocdPair { .button_dir1 = BTN_LF2, .button_dir2 = BTN_RF4, .socd_type = SOCD_2IP },
+                SocdPair { .button_dir1 = BTN_LF2, .button_dir2 = BTN_LF4, .socd_type = SOCD_2IP },
                 SocdPair { .button_dir1 = BTN_RT3, .button_dir2 = BTN_RT5, .socd_type = SOCD_2IP },
                 SocdPair { .button_dir1 = BTN_RT2, .button_dir2 = BTN_RT4, .socd_type = SOCD_2IP },
             },
